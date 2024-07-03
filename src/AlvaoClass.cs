29c94627331d78e16868a78ebccc988b7517a515
Jakub Čábera <cabera.jakub@gmail.com>
2024-07-03 05:41:24 +0200

    feat: Process properties

--- src/AlvaoClass.cs
+++ src/AlvaoClass.cs
@@ -15,7 +15,7 @@ public class AlvaoClass
     public List<string> Usings { get; set; }
 
     public Dictionary<string, string>? Constructors { get; set; }
-    public Dictionary<string, string>? Properties { get; set; }
+    public List<string> Properties { get; set; }
     public List<string> Fields { get; set; }
     public Dictionary<string, string>? Methods { get; set; }
 
@@ -23,12 +23,39 @@ public class AlvaoClass
     {
         Usings = [];
         Fields = [];
+        Properties = [];
         FullUrl = fullUrl;
         LocalHtmlFile = localHtmlFile;
         NamespaceName = namespaceName;
         Name = name;
         HtmlDocument = Helpers.LoadDocument(fullUrl, localHtmlFile);
         FinalCsFile = $"{namespaceName.Replace(".", "/")}/{name}.cs";
+    }
+
+    public void ProcessProperties()
+    {
+        var properties = HtmlDocument.DocumentNode.SelectNodes("//table[@id=\"PropertyList\"]/tr/td[2]/a");
+        if (properties == null) return;
+
+        foreach (var p in properties)
+        {
+            var propName = p.InnerText;
+            Console.WriteLine($"    Processing {propName} Property");
+
+            var propHtmlBaseFileName = p.GetAttributeValue("href", "").Split("/").Last();
+            var propLink = $"{Helpers.BASE_HTML_URL}/{propHtmlBaseFileName}";
+            var propLocalHtml = $"{Helpers.LOCAL_HTML_FOLDER}/{propHtmlBaseFileName}";
+            if (!propLink.StartsWith("https://doc.alvao")) continue;
+
+            var propDocument = Helpers.LoadDocument(propLink, propLocalHtml);
+            var propDef = propDocument.DocumentNode.SelectSingleNode("//div[@id='IDAB_code_Div1']")?.InnerText.Trim();
+            if (propDef == null) continue;
+            propDef = propDef.Replace("&lt;", "<").Replace("&gt;", ">");
+
+            if (propDef.Contains("JsonPropertyAttribute(")) Usings.Add("Newtonsoft.Json");
+
+            Properties.Add($"{propDef}");
+        }
     }
 
     public void ProcessFields()
@@ -44,9 +71,12 @@ public class AlvaoClass
             var fieldHtmlBaseFileName = f.GetAttributeValue("href", "").Split("/").Last();
             var fieldLink = $"{Helpers.BASE_HTML_URL}/{fieldHtmlBaseFileName}";
             var fieldLocalHtml = $"{Helpers.LOCAL_HTML_FOLDER}/{fieldHtmlBaseFileName}";
+            if (!fieldLink.StartsWith("https://doc.alvao")) continue;
+
             var fieldDocument = Helpers.LoadDocument(fieldLink, fieldLocalHtml);
 
-            var fieldDef = fieldDocument.DocumentNode.SelectSingleNode("//div[@id='IDAB_code_Div1']").InnerText.Trim();
+            var fieldDef = fieldDocument.DocumentNode.SelectSingleNode("//div[@id='IDAB_code_Div1']")?.InnerText.Trim();
+            if (fieldDef == null) continue;
             fieldDef = fieldDef.Replace("&lt;", "<").Replace("&gt;", ">");
 
             if (fieldName.Equals("EmailFormat"))
@@ -58,10 +88,9 @@ public class AlvaoClass
                 fieldDef = fieldDef.Replace("= \"", "= @\"");
             }
 
-            Fields.Add($"{fieldDef};");
+            Fields.Add($"{fieldDef}");
         }
     }
-
 
     public void Process()
     {
@@ -69,12 +98,12 @@ public class AlvaoClass
         Definition = HtmlDocument.DocumentNode.SelectSingleNode("//div[@id='IDAB_code_Div1']").InnerText.Trim();
         Definition = Definition.Replace("&lt;", "<").Replace("&gt;", ">");
 
-        if (Definition.Contains("TableAttribute(")) Usings.Add("using System.ComponentModel.DataAnnotations.Schema;");
-        if (Definition.Contains(": Profile")) Usings.Add("using AutoMapper;");
-        if (Definition.Contains(": tbl")) Usings.Add("using Alvao.API.Common.Model.Database;");
-        if (Definition.Contains(": vColumnLoc")) Usings.Add("using Alvao.API.Common.Model.Database;");
+        if (Definition.Contains("TableAttribute(")) Usings.Add("System.ComponentModel.DataAnnotations.Schema");
+        if (Definition.Contains(": Profile")) Usings.Add("AutoMapper");
+        if (Definition.Contains(": tbl")) Usings.Add("Alvao.API.Common.Model.Database");
+        if (Definition.Contains(": vColumnLoc")) Usings.Add("Alvao.API.Common.Model.Database");
 
-        // TODO: Process properties
+        ProcessProperties();
         ProcessFields();
         // TODO: Process constructors
         // TODO: Process properties
@@ -88,24 +117,15 @@ public class AlvaoClass
     {
         var sb = new StringBuilder();
 
-        if (Usings.Count != 0)
-        {
-            Usings.ForEach((element) =>
-            {
-                sb.AppendLine(element);
-            });
-            sb.AppendLine("");
-        }
+        if (Usings.Count != 0) Properties.ForEach(el => sb.AppendLine($"using {el};"));
+
         sb.AppendLine($"namespace {NamespaceName};");
         sb.AppendLine("");
+
         sb.AppendLine(Definition);
         sb.AppendLine("{");
-        // TODO: Properties
-        // TODO: Fields
-        foreach (var f in Fields)
-        {
-            sb.AppendLine($"    {f}");
-        }
+        Properties.ForEach(el => sb.AppendLine($"    {el}"));
+        Fields.ForEach(el => sb.AppendLine($"    {el};"));
         // TODO: Constructors
         // TODO: Methods
         sb.AppendLine("}");
