8fc19e7beebf26c5ae4fe50890063f8cdf9f4a6a
Jakub Čábera <cabera.jakub@gmail.com>
2024-07-05 22:45:40 +0200

    feat: Enable all namespaces

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 using AlvaoScapper;
 
 string[] alvaoNamespace = {
-    // "Alvao.API.AI",
-    // "Alvao.API.AI.Model",
-    // "Alvao.API.AI.Utils",
-    // "Alvao.API.AM",
+    "Alvao.API.AI",
+    "Alvao.API.AI.Model",
+    "Alvao.API.AI.Utils",
+    "Alvao.API.AM",
     "Alvao.API.AM.Exceptions",
-    // "Alvao.API.AM.Model",
-    // "Alvao.API.Common",
-    // "Alvao.API.Common.Exceptions",
-    // "Alvao.API.Common.Model",
-    // "Alvao.API.Common.Model.Database",
-    // "Alvao.API.SD",
-    // "Alvao.API.SD.Exceptions",
-    // "Alvao.API.SD.Model",
-    // "Alvao.Context",
-    // "Alvao.Context.DB"
+    "Alvao.API.AM.Model",
+    "Alvao.API.Common",
+    "Alvao.API.Common.Exceptions",
+    "Alvao.API.Common.Model",
+    "Alvao.API.Common.Model.Database",
+    "Alvao.API.SD",
+    "Alvao.API.SD.Exceptions",
+    "Alvao.API.SD.Model",
+    "Alvao.Context",
+    "Alvao.Context.DB"
 };
 
 
