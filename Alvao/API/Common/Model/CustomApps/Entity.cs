6e6abe242daab286016d4010df7e10281dc4cc1e
Jakub Čábera <cabera.jakub@gmail.com>
2024-07-09 22:28:54 +0200

    feat: Add manually create Alvao.API.Common.Model.CustomApps namespace

---
+++ Alvao/API/Common/Model/CustomApps/Entity.cs
@@ -0,0 +1,9 @@
+// CAUTION: Manually guessed object based on the documentation. It does not need to be completely accurate.
+
+namespace Alvao.API.Common.Model.CustomApps;
+
+public enum Entity
+{
+    Request,
+    Object
+}
---
+++ Alvao/API/Common/Model/CustomApps/EntityCommandShowResult.cs
@@ -0,0 +1,19 @@
+// CAUTION: Manually guessed object based on the documentation. It does not need to be completely accurate.
+
+namespace Alvao.API.Common.Model.CustomApps;
+
+public class EntityCommandShowResult
+{
+    public string Name { get; }
+    public bool Show { get; }
+    public int Position { get; }
+    public string Icon { get; }
+
+    public EntityCommandShowResult(bool show, string name, string icon, int position)
+    {
+        Show = show;
+        Name = name;
+        Icon = icon;
+        Position = position;
+    }
+}
