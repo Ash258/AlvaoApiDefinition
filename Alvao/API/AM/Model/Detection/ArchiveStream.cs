20346e4eb2e0b596e57f08dbd43512e2322612dc
Jakub Čábera <cabera.jakub@gmail.com>
2025-05-25 18:50:29 +0200

    fix(Alvao): Fix wording of manually generated classs and members

--- Alvao/API/AM/Model/Detection/ArchiveStream.cs
+++ Alvao/API/AM/Model/Detection/ArchiveStream.cs
@@ -1,6 +1,7 @@
 namespace Alvao.API.AM.Model.Detection;
 
-/// <summary>!!!CAUTION: This method is not documented. It was generated as empty, to make the project compilable</summary>
+/// <summary>!!!CAUTION: This class is not documented. It was generated as empty, to make the project compilable</summary>
 public abstract class ArchiveStream {
+    /// <summary>!!!CAUTION: This member is not documented. It was generated as empty, to make the project compilable</summary>
     public abstract bool IsUnicode { get; }
 }
