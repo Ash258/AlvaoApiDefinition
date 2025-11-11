754686ed07aff05b3db397547646c250410274a8
Jakub Čábera <cabera.jakub@gmail.com>
2025-11-11 23:25:43 +0100

    chore: Release 25.3.7. Fully compilable examples 🎉

---
+++ Alvao/Apps/API/CommandDesc.cs
@@ -0,0 +1,16 @@
+namespace Alvao.Apps.API;
+
+/// <summary>!!!CAUTION: This class is not documented. It was generated as empty, to make the project compilable<br/><br/></summary>
+/// <see href="https://doc.alvao.com/en/25/alvao-api/api/Alvao.API.Common.Model.CustomApps.CommandDesc.html"/>
+public class CommandDesc {
+    /// <see href="https://doc.alvao.com/en/25/alvao-api/api/Alvao.API.Common.Model.CustomApps.CommandDesc.html#Alvao_API_Common_Model_CustomApps_CommandDesc_icon"/>
+    public string icon;
+    /// <see href="https://doc.alvao.com/en/25/alvao-api/api/Alvao.API.Common.Model.CustomApps.CommandDesc.html#Alvao_API_Common_Model_CustomApps_CommandDesc_id"/>
+    public int id;
+    /// <see href="https://doc.alvao.com/en/25/alvao-api/api/Alvao.API.Common.Model.CustomApps.CommandDesc.html#Alvao_API_Common_Model_CustomApps_CommandDesc_name"/>
+    public string name;
+    /// <see href="https://doc.alvao.com/en/25/alvao-api/api/Alvao.API.Common.Model.CustomApps.CommandDesc.html#Alvao_API_Common_Model_CustomApps_CommandDesc_position"/>
+    public int position;
+
+    public CommandDesc(int id, string name, int position, string icon) { }
+}
