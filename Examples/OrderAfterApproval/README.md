b577a0618e45c2f36a8f44532e0adfce7b024c09
Jakub Čábera <cabera.jakub@gmail.com>
2025-11-11 22:32:40 +0100

    feat(Examples): Implement OrderAfterApproval

---
+++ Examples/OrderAfterApproval/README.md
@@ -0,0 +1,5 @@
+# Order After Approval
+
+Source: <https://doc.alvao.com/en/25/modules/alvao-am-custom-apps/applications/templates/#order-after-approval>
+
+When a ticket is approved in Purchase service by all approvers, the ticket goes into the Order status.
---
+++ Examples/OrderAfterApproval/Settings.cs
@@ -0,0 +1,7 @@
+public class Settings 
+{
+    public static string ActionName = "OrderAfterApproval";
+    public static string ServiceName = "Purchase"; // The name of the service where the approval is taking place
+    public static string State = "Purchase approval"; // The name of the status where the approval is taking place.
+    public static string TargetState = "Order"; // The name of the status to enter after approval.
+}
